4263e54841606cb649d394d92d30b7fed28677b8
VuCuongHD96 <xuancuonghd1102@gmail.com>
2023-10-13 21:58:32 +0700

    Initial Commit

---
+++ SpreadSheetView/SpreadSheetViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpreadSheetViewApp.swift
+//  SpreadSheetView
+//
+//  Created by Work on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpreadSheetViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
